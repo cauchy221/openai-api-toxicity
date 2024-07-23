d97def3d453dfdef427ac2257718ee096b82c293
Xinyue Liu <56873805+cauchy221@users.noreply.github.com>
2024-07-22 22:49:33 -0500

    add try/except for error request

--- toxic.py
+++ toxic.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 import requests
 import json
 from time import sleep
+from tqdm import tqdm
 
 load_dotenv()
 
@@ -13,22 +14,36 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY")
 )
 
-def get_toxicity_score(text):
+def get_toxicity_score(text, max_tries=3):
     comment = {
         'comment': {'text': text},
         'languages': ['en'],
         'requestedAttributes': {'TOXICITY': {}}
     }
     url = (f'https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key={os.environ.get("PERSPECTIVE_API_KEY")}')
-    response = requests.post(url, data=json.dumps(comment))
-    score = response.json()['attributeScores']['TOXICITY']['summaryScore']['value']
-    return score
+
+    for attempt in range(max_tries):
+        try:
+            response = requests.post(url, data=json.dumps(comment))
+            response = response.json()
+            if 'attributeScores' in response and 'TOXICITY' in response['attributeScores']:
+                score = response['attributeScores']['TOXICITY']['summaryScore']['value']
+                return score
+            else:
+                print(f"Retrying... {1 + attempt}/{max_tries}")
+                sleep(2)
+        except Exception as e:
+            print(f"Error: {e}")
+            print(f"Retrying... {1 + attempt}/{max_tries}")
+            sleep(2)
+
+    return None
 
 
 if __name__ == '__main__':
 
     # Hyperparameters
-    N = 100
+    N = 1000
     OPENAI_MODEL = "gpt-3.5-turbo-instruct"
 
     # Load the dataset
@@ -37,10 +52,10 @@ if __name__ == '__main__':
     # Loop through the dataset
     toxic_scores = 0
 
-    for prompt in dataset:
+    for prompt in tqdm(dataset):
         response = client.completions.create(
             model=OPENAI_MODEL,
-            prompt=prompt
+            prompt=prompt['text']
         )
         response_text = response.choices[0].text
 
@@ -49,4 +64,4 @@ if __name__ == '__main__':
         toxic_scores += score
         sleep(2)
 
-    print(f"Average toxicity score: {toxic_scores / N}")
+    print(f"Average toxicity score of model {OPENAI_MODEL} with {N} data is {toxic_scores / N}")
